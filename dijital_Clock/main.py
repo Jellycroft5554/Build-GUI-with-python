6a447191b05a9bc529ca4dd21d0c39257080fb20
Jellycroft5554 <ryan.injarie@hotmail.com>
2025-12-03 19:25:08 +0000

    Add files via upload

--- dijital_Clock/main.py
+++ dijital_Clock/main.py
@@ -1,16 +1,23 @@
 from tkinter import *
 from tkinter.ttk import *
 from time import strftime
+import random
 
 root = Tk()
 root.title("Clock")
 
+colours=["red", "orange", "yellow", "green", "blue", "pink", "purple"]
+
 def time():
     string = strftime("%H:%M:%S %p")
-    lbl.config(text=string)
+    lbl.config(text=string, background=random.choice(colours), foreground=random.choice(colours))
     lbl.after(1000, time)
 
 
+
+
 lbl = Label(root, font=("calibri",40,"bold"), background="orange", foreground="white")
+lbl.pack(anchor="center")
+
 time()
 mainloop()
