d78ac84e1f3ae8ea04b27929877935d218cf8a85
Jellycroft5554 <ryan.injarie@hotmail.com>
2026-01-14 19:05:52 +0000

    Update multi.py

--- multi.py
+++ multi.py
@@ -1,23 +1,42 @@
-from tkinter import *
+import tkinter
+import math
+import tkinter.messagebox
+import random
 
-window = Tk()
-window.geometry("500x500")
-window.config(background=    "#FFFFFF")
-
-frame=Frame(window, bg="#000000")
-frame.pack(padx=20)
-
-label=Label(frame, text="hello", width=20,height=3)
-label.pack(pady=20, padx=20, side=RIGHT)
-
-label=Label(frame, text="hey", width=20,height=3)
-label.pack(pady=20, padx=20, side=LEFT)
-
-frame2=Frame(window, bg="#FF0000")
-frame2.pack(padx=20)
-
-label=Label(frame2, text="hello", width=20,height=3)
-label.pack(pady=20, padx=20, side=RIGHT)
+root = tkinter.Tk()
+root.geometry("600x500")
+root.title("Multiplication Table")
 
 
-window.mainloop()
+entry = tkinter.Entry(root)
+entry.grid(column=0, row=0, padx=80, pady=80)
+
+
+text = tkinter.Label(root, text= "X")
+text.grid(column=1, row=0)
+
+entry2 = tkinter.Entry(root)
+entry2.grid(column=2, row=0, padx=60, pady=80)
+
+
+
+def solve():
+
+    pro = entry.get()
+    pro2 = entry2.get()
+
+    product = float(pro)
+    product2 = float(pro2)
+
+    answer = product*product2
+
+    formula = product, "*" ,product2, "=" ,answer
+
+    answerbox = tkinter.Label(root, text=formula)
+    answerbox.grid(column=2, row=1, columnspan=2)
+
+    
+solver= tkinter.Button(root, text="solve",command=solve)
+solver.grid(column=0, row=1)
+
+root.mainloop()
