f6f7b9b69b50b830bd0d60a04eae3a28f79a8a8d
Jellycroft5554 <ryan.injarie@hotmail.com>
2026-03-18 19:12:14 +0000

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,79 +1,57 @@
+import cv2
+import numpy as np
+from tkinter import *
 import tkinter
 import math
 import tkinter.messagebox
 
 
-root = tkinter.Tk()
-root.geometry("600x500")
-root.title("Notes")
+root = Tk()
+root.title("Image Buttons")
+root.geometry("300x200")
 
 
-def run():
-
-    username_1.get()
-    username_1.get()
+img1 = cv2.imread("pika.png")
+img2 = cv2.imread("bee.png")
 
 
-def run1():
+img2 = cv2.resize(img2, (img1.shape[1], img1.shape[0]))
 
 
-    if username_1.get() == username_12.get() and password_1.get() == password_12.get():
-
-        print("hi " + username_1.get())
-
-        add=tkinter.Button(root, text = "Add Images")
-        add.grid(row=4, column=0)
-
-        subtract=tkinter.Button(root, text = "subtract")
-        subtract.grid(row=4, column=1)
-
-        resize=tkinter.Button(root, text = "resize")
-        resize.grid(row=4, column=2)
-
-    
-    else:
-
-        print("ERROR: incorrect credencials " + username_1.get())
-        
-#signin
-
-sign=tkinter.Label(root, text="sign up")
-sign.grid(row=0, column=0, pady=20)
 
 
-username1=tkinter.Label(root, text="username")
-username1.grid(row=1, column=0, pady=20)
-
-username_12=tkinter.Entry(root)
-username_12.grid(row=1, column=1, pady=20)
-
-
-password1=tkinter.Label(root, text="password")
-password1.grid(row=2, column=0, pady=20)
-
-password_12=tkinter.Entry(root)
-password_12.grid(row=2, column=1, pady=20)
+def resize_image():
+    img1 = cv2.imread("bee.png")
+    Resized = cv2.resize(img1, (200, 400))
+    cv2.imshow("Resized Image", Resized)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
-signin=tkinter.Button(root, text="signin", command=run)
-signin.grid(row=2, column=2, pady=20)
-
-#login
-
-log=tkinter.Label(root, text="log in")
-log.grid(row=0, column=3, pady=20)
+def add_images():
+    added = cv2.add(img1, img2)
+    cv2.imshow("Added Image", added)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
-username_1=tkinter.Entry(root)
-username_1.grid(row=1, column=3, pady=20)
+def subtract_images():
+    subtracted = cv2.subtract(img1, img2)
+    cv2.imshow("Subtracted Image", subtracted)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
-password_1=tkinter.Entry(root)
-password_1.grid(row=2, column=3, pady=20)
+Resize=Button(root, text="Resize", command=resize_image)
+Resize.pack(pady=10)
 
 
-login=tkinter.Button(root, text="login", command=run1)
-login.grid(row=2, column=5, pady=20)
+Add=Button(root, text="Add", command=add_images)
+Add.pack(pady=10)
+
+
+Sub=Button(root, text="Subtract", command=subtract_images) 
+Sub.pack(pady=10)
 
 
 root.mainloop()
